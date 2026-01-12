1b73fcd1e587ec5c748bb003982daa3b305b2279
DESKTOP-091MBB9\Garison4ik <staskalyn8754@gmail.com>
2026-01-12 20:23:45 +0200

    create Exclusive deals section

--- js/page/homePage.js
+++ js/page/homePage.js
@@ -1,10 +1,2 @@
-import { renderCarousel } from "../component/carousel.js";
-import { exclusiveDealsData } from "../../data/Exclusive deals/exclusiveDealsData.js";
-import { carouselExclusiveDealsCard } from "../component/carousel.js";
-const carouselExclusiveDealsInner = document.querySelector('.js-carousel-exclusive-deals-inner');
 
-
-const carouselCardData = carouselExclusiveDealsCard(exclusiveDealsData);
-
-renderCarousel(carouselExclusiveDealsInner,carouselCardData);
 
